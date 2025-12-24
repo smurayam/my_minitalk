5c3c7914c4a2a0eea6678fc427667a1627e61ba1
Sumire Murayama <smurayam@c1r7s1.42tokyo.jp>
2025-12-24 17:47:33 +0900

    dofixmakefile

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: smurayam <smurayam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 03:23:27 by nnnya             #+#    #+#             */
-/*   Updated: 2025/12/24 00:17:12 by smurayam         ###   ########.fr       */
+/*   Updated: 2025/12/24 17:43:05 by smurayam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,9 @@ int	send_str(char *str, int pid)
 		while (i >= 0)
 		{
 			if (*str & (1 << i))
-				kill(pid, SIGUSR2) else kill(pid, SIGUSR1);
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
 			while (!g_server_sig)
 				;
 			g_server_sig = 0;
@@ -34,6 +36,11 @@ int	send_str(char *str, int pid)
 	}
 }
 
+void	handler(int sig)
+{
+	g_server_sig = sig;
+}
+
 int	main(int ac, char **av)
 {
 	if (ac != 3)
@@ -41,5 +48,9 @@ int	main(int ac, char **av)
 		ft_printf("Usage: %s <Server PID> <Stinrg to send>\n");
 		return (1);
 	}
-	signal(SIGUSR)
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	g_server_sig = 0;
+	send_by_bits(ft_atoi(av[1], av[2]));
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,14 @@
 /*   By: smurayam <smurayam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 03:21:48 by nnnya             #+#    #+#             */
-/*   Updated: 2025/12/23 22:05:10 by smurayam         ###   ########.fr       */
+/*   Updated: 2025/12/24 17:45:54 by smurayam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "Libft01/libft.h"
+# include "printf/ft_printf.h"
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
